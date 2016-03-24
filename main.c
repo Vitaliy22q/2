eb3435fb9c574ee6ca6c1d821a6038b13bd1f31a
Vitaliy22q <Vitaliy22q@list.ru>
2016-03-24 21:19:34 +0200

    Added files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main ()
+{ 
+   
+    int n, i = 0;
+    printf("Vvedite naturalnoe chislo\n");
+    scanf ("%d",&n);
+   
+    while (n!= 0) {
+         n = n / 10;
+         i++;
+}
+printf("Vashe chislo imeet %d znakow.\n",i);
+
+system ("pause");
+
+	return 0;
+}
+
+
