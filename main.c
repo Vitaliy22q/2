b43972b31aa9564c96b4763106a569bfd7929a4f
Vitaliy22q <Vitaliy22q@list.ru>
2016-03-31 22:47:59 +0300

    Added files via upload

--- main.c
+++ main.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-
-int main ()
-{ 
-   
-    int n, i = 0;
-    printf("Vvedite naturalnoe chislo\n");
-    scanf ("%d",&n);
-   
-    while (n!= 0) {
-         n = n / 10;
-         i++;
-}
-printf("Vashe chislo imeet %d znakow.\n",i);
-
-system ("pause");
-
+int main(int argc, char *argv[]) {
+	int m, n;
+	double nod1, nod2, nod;
+	m>=n;
+	printf("\nVvedite n:");
+	scanf("%d", &n);
+	printf("\nVvedite m:");
+	scanf("%d", &m);
+	
+	for ( nod1=n;nod2=m;nod1!=0 ){
+		nod = nod2 % nod1;
+		nod2=nod1;
+		nod1=nod;
+	}
+	printf("NOD = %d", nod2);
+	
 	return 0;
 }
-
-
